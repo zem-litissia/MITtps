592114b304a8f5f7706054594102f8ddb5500c7c
zem-litissia <litissiazemmour@gmail.com>
2025-11-04 20:28:00 +0100

    Update isp.py After isp

--- SOLID/isp.py
+++ SOLID/isp.py
@@ -1,19 +1,29 @@
-class Printer:
+# Separate Interfaces
+class Printable:
     def print_document(self, document):
         pass
+class Scannable:
     def scan_document(self, document):
         pass
+class Faxable:
     def fax_document(self, document):
         pass
-class BasicPrinter(Printer):
+class BasicPrinter(Printable):
+    def print_document(self, document):
+        print(f"Printing: {document}")
+
+class AdvancedPrinter(Printable, Scannable, Faxable):
     def print_document(self, document):
         print(f"Printing: {document}")
     def scan_document(self, document):
-        raise NotImplementedError("This printer cannot scan.")
+        print(f"Scanning: {document}")
     def fax_document(self, document):
-        raise NotImplementedError("This printer cannot fax.")
-if __name__ == '__main__':
+        print(f"Faxing: {document}")
+if __name__ == "__main__":
     basic_printer = BasicPrinter()
-    basic_printer.print_document("Document2.pdf")
-    basic_printer.scan_document("Document2.pdf")  
-    basic_printer.fax_document("Document2.pdf")   
+    basic_printer.print_document("Doc1.pdf")
+
+    advanced_printer = AdvancedPrinter()
+    advanced_printer.print_document("Doc2.pdf")
+    advanced_printer.scan_document("Doc2.pdf")
+    advanced_printer.fax_document("Doc2.pdf")
