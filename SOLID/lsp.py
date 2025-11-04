5a9bd73c8b4791651f225613110996cd681c69b3
zem-litissia <litissiazemmour@gmail.com>
2025-11-04 20:16:43 +0100

    Update lsp.py after lsp

--- SOLID/lsp.py
+++ SOLID/lsp.py
@@ -1,37 +1,31 @@
-class Rectangle:
-    def set_width(self, width):
-        self.width = width
 
-    def set_height(self, height):
+class Shape:
+    def get_area(self):
+        raise NotImplementedError("Must be implemented by the subclass")
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
         self.height = height
 
     def get_area(self):
         return self.width * self.height
 
-class Square(Rectangle):  
-    def set_width(self, width):
-        self.width = width
-        self.height = width 
+class Square(Shape):
+    def __init__(self, side_length):
+        self.side_length = side_length
 
-    def set_height(self, height):
-        self.height = height
-        self.width = height  
+    def get_area(self):
+        return self.side_length * self.side_length
 
 
-def print_area(rectangle):
-   
-    print(f"Area: {rectangle.get_area()}")
+# Client code
+def print_area(shape):
+    print(f"Area: {shape.get_area()}")
 
 
-if __name__ == '__main__':
-   
-    rect = Rectangle()
-    rect.set_width(5)
-    rect.set_height(10)
-
-    square = Square()
-    square.set_width(5)
-    square.set_height(10)
+if __name__ == "__main__":
+    rect = Rectangle(5, 10)
+    square = Square(5)
 
     print_area(rect)    
     print_area(square)  
