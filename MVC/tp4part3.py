4c83d98eda5d5adac394e0e87b3806afd91e7c88
zem-litissia <litissiazemmour@gmail.com>
2025-11-13 17:44:20 +0100

    add part 3

---
+++ MVC/tp4part3.py
@@ -0,0 +1,105 @@
+class model:
+    """ data model class"""
+    def __init__(self):
+        self.directory = [
+            {"firstname":'Ahmed', "familyname":'Mahdi', 'tel':'0778787887'},
+            {"firstname":'Mohamed', "familyname":'Mahdi','tel':'0778787887'},
+            {"firstname":'Mounir', "familyname":'Katibi','tel':'0778787887'},
+            {"firstname":'Noui', "familyname":'Brahimi','tel':'0778787887'},
+        ]
+
+    def search(self, nom):
+        """ lookup for a tel by lastname"""
+        persons = []
+        for persn in self.directory:
+            if persn["familyname"] == nom:
+                persons.append(persn)
+        return persons
+
+    def get_all(self):
+        """ Return all people"""
+        persons = []
+        for persn in self.directory:
+            persons.append(persn)
+        return persons
+    def add(self, name, firstname, tel):
+        """Add a person"""
+        person = {}
+        person["familyname"] = name
+        person["firstname"] = firstname
+        person["tel"] = tel
+        self.directory.append(person)
+class view:
+    def __init__(self):
+        pass
+    def input(self):
+        """ retrieve the name to search for"""
+        print("Finding a phone")
+        name = input("Give a name: ")
+        return name
+    
+    def output(self, persons):
+        """ display information from a list of persons"""
+        print("List of found names")
+        print(" %d found people " % len(persons))
+        for pers in persons:
+            print(pers["familyname"], pers["firstname"], pers['tel'])
+
+    def output_all(self, persons):
+        """ display all people in the directory"""
+        print("\nAll people in the directory:")
+        for pers in persons:
+            print(pers["familyname"], pers["firstname"], pers['tel'])
+    def input_add(self):
+        """Get info to add a new person"""
+        print("Add a person")
+        print("Give a name")
+        name = input()
+        print("Give a first name")
+        firstname = input()
+        print("Give a telephone number")
+        tel = input()
+        return (name, firstname, tel)
+class Controller:
+    def __init__(self):
+        """ controller constructor """
+        self.data_model = model()  # initialisation du modèle
+        self.viewer = view()       # initialisation de la vue
+
+    def search(self):
+        """ search a name """
+        name = self.viewer.input()
+        persons = self.data_model.search(name)
+        self.viewer.output(persons)
+
+    def display_all(self):
+        """ display all people """
+        persons = self.data_model.get_all()
+        self.viewer.output_all(persons)
+    def add(self):
+        """Add a new person"""
+        name, firstname, tel = self.viewer.input_add()
+        self.data_model.add(name, firstname, tel)
+        persons = self.data_model.get_all()
+        self.viewer.output(persons)
+if __name__ == '__main__':
+    contro = Controller()
+    
+    while True:
+        print("\nMenu:")
+        print("1. Search by family name")
+        print("2. Display all people")
+        print("3. Add a new person")
+        print("4. Exit")
+        choice = input("Choose an option: ")
+
+        if choice == '1':
+            contro.search()
+        elif choice == '2':
+            contro.display_all()
+        elif choice == '3':
+            contro.add()
+        elif choice == '4':
+            break
+        else:
+            print("Invalid choice. Try again.")
