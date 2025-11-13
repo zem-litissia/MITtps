64e5d3c4e0ec5dacfa755aed2fec9c8ecb83251d
zem-litissia <litissiazemmour@gmail.com>
2025-11-13 17:20:51 +0100

    modified controller

--- MVC/tp4part2.py
+++ MVC/tp4part2.py
@@ -29,28 +29,49 @@ class model:
 class view:
     def __init__(self,):
         pass
+
     def input(self,):
         """ retrieve the name to search for"""
         print("Finding a phone")
         print("Give a name")
         name = input()
         return name
+
     def output(self, persons):
         """ display information from a list of persons"""
         print("List of found names")
         print(" %d found people "%len(persons))
         for pers in persons:
-             print(pers["familyname"], pers["firstname"], pers['tel'])  
+             print(pers["familyname"], pers["firstname"], pers['tel'])
+             
+    def output_all(self, persons):
+        """ display all people in the directory"""
+        print("\nAll people in the directory:")
+        for pers in persons:
+             print(pers["familyname"], pers["firstname"], pers['tel'])
+
 def controller():
-        # This program allows you to search the number
-        # in a table by its name, and display
-        # read data from the keyboard
-            data_model = model()
-            myviewer = view()
-            """ search a name """
+    data_model = model()
+    myviewer = view()
+
+    while True:
+        print("\nMenu:")
+        print("1. Search by family name")
+        print("2. Display all people")
+        print("3. Exit")
+        choice = input("Choose an option: ")
+
+        if choice == '1':
             name = myviewer.input()
             persons = data_model.search(name)
             myviewer.output(persons)
+        elif choice == '2':
+            persons = data_model.get_all()
+            myviewer.output_all(persons)
+        elif choice == '3':
+            break
+        else:
+            print("Invalid choice. Try again.")
           
 if __name__ == '__main__':
         controller()
