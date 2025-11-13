332fdb41abe4a71ec0c8309d0f3a9b603ee20f29
zem-litissia <litissiazemmour@gmail.com>
2025-11-13 17:31:12 +0100

    add new controller

--- MVC/tp4part2.py
+++ MVC/tp4part2.py
@@ -1,59 +1,69 @@
 class model:
-     """ data model class"""
-     def __init__(self):
+    """ data model class"""
+    def __init__(self):
         self.directory = [
-        {"firstname":'Ahmed', "familyname":'Mahdi', 'tel':'0778787887'},
-        {"firstname":'Mohamed', "familyname":'Mahdi','tel':'0778787887'},
-        {"firstname":'Mounir', "familyname":'Katibi','tel':'0778787887'},
-        {"firstname":'Noui', "familyname":'Brahimi','tel':'0778787887'},
+            {"firstname":'Ahmed', "familyname":'Mahdi', 'tel':'0778787887'},
+            {"firstname":'Mohamed', "familyname":'Mahdi','tel':'0778787887'},
+            {"firstname":'Mounir', "familyname":'Katibi','tel':'0778787887'},
+            {"firstname":'Noui', "familyname":'Brahimi','tel':'0778787887'},
         ]
-     def search(self, nom):
+
+    def search(self, nom):
         """ lookup for a tel by lastname"""
-        # found people list
         persons = []
         for persn in self.directory:
-        # show all people with the given name
             if persn["familyname"] == nom:
-                 persons.append(persn)
-            # return a list of dictionaries
+                persons.append(persn)
         return persons
-     def get_all(self):
-        """ Find a tel by name"""
-        # List of found people
+
+    def get_all(self):
+        """ Return all people"""
         persons = []
         for persn in self.directory:
-        # display all people
-             persons.append(persn)
-        # a table with field names
+            persons.append(persn)
         return persons
 class view:
-    def __init__(self,):
+    def __init__(self):
         pass
-
-    def input(self,):
+    def input(self):
         """ retrieve the name to search for"""
         print("Finding a phone")
-        print("Give a name")
-        name = input()
+        name = input("Give a name: ")
         return name
-
+    
     def output(self, persons):
         """ display information from a list of persons"""
         print("List of found names")
-        print(" %d found people "%len(persons))
+        print(" %d found people " % len(persons))
         for pers in persons:
-             print(pers["familyname"], pers["firstname"], pers['tel'])
-             
+            print(pers["familyname"], pers["firstname"], pers['tel'])
+
     def output_all(self, persons):
         """ display all people in the directory"""
         print("\nAll people in the directory:")
         for pers in persons:
-             print(pers["familyname"], pers["firstname"], pers['tel'])
+            print(pers["familyname"], pers["firstname"], pers['tel'])
 
-def controller():
-    data_model = model()
-    myviewer = view()
+class Controller:
+    def __init__(self):
+        """ controller constructor """
+        self.data_model = model()  # initialisation du modèle
+        self.viewer = view()       # initialisation de la vue
 
+    def search(self):
+        """ search a name """
+        name = self.viewer.input()
+        persons = self.data_model.search(name)
+        self.viewer.output(persons)
+
+    def display_all(self):
+        """ display all people """
+        persons = self.data_model.get_all()
+        self.viewer.output_all(persons)
+
+if __name__ == '__main__':
+    contro = Controller()
+    
     while True:
         print("\nMenu:")
         print("1. Search by family name")
@@ -62,16 +72,10 @@ def controller():
         choice = input("Choose an option: ")
 
         if choice == '1':
-            name = myviewer.input()
-            persons = data_model.search(name)
-            myviewer.output(persons)
+            contro.search()
         elif choice == '2':
-            persons = data_model.get_all()
-            myviewer.output_all(persons)
+            contro.display_all()
         elif choice == '3':
             break
         else:
             print("Invalid choice. Try again.")
-          
-if __name__ == '__main__':
-        controller()
