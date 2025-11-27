ebed7b9765d0630b947bd9428866583dd4a63ea9
bochra0T <uxe.0710@gmail.com>
2025-11-27 21:50:41 +0100

    Update without_d_p1.py

--- pattrens/without_d_p1.py
+++ pattrens/without_d_p1.py
@@ -5,7 +5,7 @@ import sys
 
 #class Shape:
  #   def draw(self): pass
- #   def erase(self): pass
+  #  def erase(self): pass
 
 class Circle(Shape):
     def draw(self): 
@@ -19,12 +19,28 @@ class Square(Shape):
     def erase(self): 
         print("Square.erase")
 
+class Rectangle(Shape):
+    def draw(self): 
+        print("Rectangle.draw")
+    def erase(self): 
+        print("Rectangle.erase")
+
+class Triangle(Shape):
+    def draw(self): 
+        print("Triangle.draw")
+    def erase(self): 
+        print("Triangle.erase")
+
 if __name__ == "__main__":
-    for type in ("Circle", "Square", "Circle", "Square"):
+    for type in ("Circle", "Square", "Rectangle", "Triangle"):
         if type == "Circle":
             shape = Circle()
         elif type == "Square":
             shape = Square()
+        elif type == "Rectangle":
+            shape = Rectangle()
+        elif type == "Triangle":
+            shape = Triangle()
         else:
             print("Bad shape creation: " + type)
             sys.exit()
